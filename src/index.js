42d0c0147e635227182e19113972dbf6cd8a9bb6
김선희 <sunkim@hist.co.kr>
2022-05-09 13:43:39 +0900

    스토어 등록 및 리덕스 개발자도구 적용

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// **** (1) createStore 와 루트 리듀서 불러오기
+import { createStore } from 'redux';
+import rootReducer from './store/modules';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// **** (2) 스토어를 만들고 현재 값 확인해보기
+// const store = createStore(rootReducer);
+// console.log(store.getState());
+
+// **** (3) 리덕스 개발자도구 적용
+const devTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = createStore(rootReducer, devTools);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
