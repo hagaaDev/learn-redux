c3f645a884ee636e43170113ccbe276a4dde5eec
김선희 <sunkim@hist.co.kr>
2022-05-10 17:30:24 +0900

    handleActions를 활용한 리듀서 함수 작성

--- src/store/modules/waiting.js
+++ src/store/modules/waiting.js
@@ -1,4 +1,4 @@
-import { createAction } from 'redux-actions'; // **** (2)
+import { createAction, handleActions } from 'redux-actions'; // **** (2) createAction 불러오기
 
 const CHANGE_INPUT = 'waiting/CHANGE_INPUT'; // 인풋 값 변경
 const CREATE = 'waiting/CREATE'; // 명단에 이름 추가
@@ -39,4 +39,53 @@ export const create = createAction(CREATE, (text) => ({ text, id: id++ }));
 export const enter = createAction(ENTER, (id) => id);
 export const leave = createAction(LEAVE, (id) => id);
 
-export default handleActions({});
+// **** (6) 초기 상태 정의
+const initialState = {
+  input: '',
+  list: [
+    {
+      id: 0,
+      name: '홍길동',
+      entered: true,
+    },
+    {
+      id: 1,
+      name: '콩쥐',
+      entered: false,
+    },
+    {
+      id: 2,
+      name: '팥쥐',
+      entered: false,
+    },
+  ],
+};
+
+// **** (7) handleActions 로 리듀서 함수 작성
+export default handleActions(
+  {
+    [CHANGE_INPUT]: (state, action) => ({
+      ...state,
+      input: action.payload,
+    }),
+    [CREATE]: (state, action) => ({
+      ...state,
+      list: state.list.concat({
+        id: action.payload.id,
+        name: action.payload.text,
+        entered: false,
+      }),
+    }),
+    [ENTER]: (state, action) => ({
+      ...state,
+      list: state.list.map((item) =>
+        item.id === action.payload ? { ...item, entered: !item.entered } : item
+      ),
+    }),
+    [LEAVE]: (state, action) => ({
+      ...state,
+      list: state.list.filter((item) => item.id !== action.payload),
+    }),
+  },
+  initialState
+);
