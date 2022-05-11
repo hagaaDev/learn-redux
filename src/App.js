094bde2cfc165985aa71a2a45c30a307b4f1a8d5
김선희 <sunkim@hist.co.kr>
2022-05-11 09:40:13 +0900

    대기자 명단 이벤트 추가

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import WaitingList from './components/WaitingList';
 import PaletteContainer from './containers/PaletteContainer'; // **** (1) 불러오기
 import CounterContainer from './containers/CounterContainer'; // **** (3) 불러오기
+import WaitingListContainer from './containers/WaitingListContainer'; // **** (5) 불러오기
 
 class App extends Component {
   render() {
@@ -13,7 +14,8 @@ class App extends Component {
         <PaletteContainer /> {/* **** (2) 대체하기 */}
         <CounterContainer /> {/* **** (4) 대체하기 */}
         {/* <Counter value={0} color="red" /> */}
-        <WaitingList />
+        <WaitingListContainer /> {/* **** (6) 교체하기 */}
+        {/* <WaitingList /> */}
       </div>
     );
   }
